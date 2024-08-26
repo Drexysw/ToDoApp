66d6c8b75ee1ee4785718a873fed092d88bd5fe2
Drexysw <knevdani@gmail.com>
2024-08-26 19:23:54 +0300

    clean architecture template created

--- Portfolio.Server/Program.cs
+++ Portfolio.Server/Program.cs
@@ -1,3 +1,6 @@
+using ToDoApp.Application;
+using ToDoApp.Infrastructure;
+using ToDoApp.Presentation;
 
 namespace Portfolio.Server
 {
@@ -7,12 +10,15 @@ namespace Portfolio.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.
+                AddApplication()
+                .AddInfrastructure()
+                .AddPresentation();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
---
+++ ToDoApp.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(cofiguration =>
+                cofiguration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
+    }
+}
